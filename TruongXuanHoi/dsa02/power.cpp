8853f86bc9ce61e814c5f7a36ef569318c1637b0
truongxuanhoi <truongxuanhoi2710@gmail.com>
2018-02-05 20:36:15 +0700

    Bai tap tuan 02

---
+++ TruongXuanHoi/dsa02/power.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	
+	int x , n;
+	cout << " Nhap x:" ;
+	cin >> x;
+	cout << " Nhap n:" ;
+	cin >> n;
+	if (n>=0){
+		cout << " Gia tri p(x,n):" << pow(x,n)<< endl;
+	}
+	else{
+		return 1;
+		}
+}
+	
+	
+	
