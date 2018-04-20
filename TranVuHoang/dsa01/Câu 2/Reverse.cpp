46dd9c8ec18f23072d8cf84860b03fc2f685fa9d
TranVuHoang <tranvuhoangjr@gmail.com>
2018-04-20 16:47:04 +0700

    add dsa01

---
+++ TranVuHoang/dsa01/Câu 2/Reverse.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+using namespace std;
+int main(){
+	vector<int> arr;
+
+	ifstream input  ("DAYSO2.txt");
+	ofstream output ("KETQUA1.txt");
+
+	int number;
+
+	while(!input.eof())
+    {
+		input >> number;
+		arr.push_back(number);
+	}
+	for(int i = arr.size() - 1; i >= 0; i--)
+		cout << arr.at(i) << endl;
+	for(int i = arr.size() - 1; i >= 0; i--)
+		output << arr.at(i) << endl;
+	return 0;
+}
